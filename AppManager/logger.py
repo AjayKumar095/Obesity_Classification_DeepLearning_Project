9cbecac2112d6a6c93fc190ec987fdfdef4b4126
Ajay <jaykumar002kori@gmail.com>
2024-11-17 21:07:05 +0530

    commit

--- AppManager/logger.py
+++ AppManager/logger.py
@@ -1,6 +1,24 @@
 ### This file contain custom logging system.
 
-## Sample method 
+from datetime import datetime
+import logging
+import os
 
-def greet(name):
-    return f'Hello, {name}'
+## create the log folder, where all the logs are stored.
+log_folder = os.path.join(os.getcwd(), 'Logs')
+
+if not os.path.exists(log_folder):
+    os.mkdir(log_folder)
+
+
+## Define the log file path.
+log_filename = datetime.now().strftime('logfile_%Y-%m-%d.log')
+log_file_path = os.path.join(log_folder, log_filename)
+
+## config the basic logger
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename=log_file_path,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s")
+
+logger = logging.getLogger(__name__)
